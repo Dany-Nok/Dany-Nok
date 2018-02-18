65c63c6ea2d7244c705661e9b8c896a213317123
Dany-Nok <daniela_gallardo_m@hotmail.com>
2018-02-18 20:31:45 -0300

    Maquetado html y css portafolio (en construcción)

---
+++ assets/js/portafolio-app.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  /* $('.parallax').parallax();
+  $('.target').pushpin({
+    top: 0,
+    bottom: 1000,
+    offset: 0
+  });
+});
+
+$('.pushpin-demo-nav').each(function() {
+  var $this = $(this);
+  var $target = $('#' + $(this).attr('data-target'));
+  $this.pushpin({
+    top: $target.offset().top,
+    bottom: $target.offset().top + $target.outerHeight() - $this.height()
+  }); */
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+});
---
+++ assets/js/typed-text.js
@@ -0,0 +1,11 @@
+// ANIMACION TEXTO SECCION "INTRO"
+var typed3 = new Typed('#typed-strings', {
+  strings: ['$> _', '$> never.stop.watching(...)_', '$> never.stop.learning(...)_',
+    '$> never.stop.trying(...)_', '$> never.stop.be-curious(...)_',
+    '$> never.give-up(!) :)_'
+  ],
+  typeSpeed: 40,
+  backSpeed: 40,
+  smartBackspace: true, // this is a default
+  loop: true
+});
